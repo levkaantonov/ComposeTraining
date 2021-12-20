f6cbe9da6d863d20b87f42fe07269456377d1afd
levkaantonov <levkaantonov@gmail.com>
2021-12-20 20:33:18 +0300

    simple animation

--- app/src/main/java/levkaantonov/com/study/composetraining/MainActivity.kt
+++ app/src/main/java/levkaantonov/com/study/composetraining/MainActivity.kt
@@ -3,19 +3,41 @@ package levkaantonov.com.study.composetraining
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import levkaantonov.com.study.composetraining.ui.theme.ComposeTrainingTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeTrainingTheme {
-                Surface(color = MaterialTheme.colors.background) {
+            var sizeState by remember {
+                mutableStateOf(200.dp)
+            }
+            val size by animateDpAsState(targetValue = sizeState)
+            Box(
+                modifier = Modifier
+                    .size(size)
+                    .background(Color.Red),
+                contentAlignment = Alignment.Center
+            ) {
+                Button(
+                    onClick = {
+                        sizeState += 50.dp
+                    }
+                ) {
+                    Text(text = "Increase size")
                 }
             }
         }
