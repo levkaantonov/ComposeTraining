b26e4735ed6f13191f8dc4936986b1ad829adeb9
levkaantonov <levkaantonov@gmail.com>
2021-12-20 14:04:04 +0300

    added list of items

--- app/src/main/java/levkaantonov/com/study/composetraining/MainActivity.kt
+++ app/src/main/java/levkaantonov/com/study/composetraining/MainActivity.kt
@@ -3,19 +3,36 @@ package levkaantonov.com.study.composetraining
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import levkaantonov.com.study.composetraining.ui.theme.ComposeTrainingTheme
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeTrainingTheme {
-                Surface(color = MaterialTheme.colors.background) {
+            val scrollSate = rememberScrollState()
+            Column(
+                modifier = Modifier.verticalScroll(scrollSate)
+            ) {
+                for (i in 1..50) {
+                    Text(
+                        text = "Item $i",
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 24.dp)
+                    )
                 }
             }
         }
