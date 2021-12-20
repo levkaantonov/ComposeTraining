b8ba82d317e80d8f75603dab2d2a87b173c899f6
levkaantonov <levkaantonov@gmail.com>
2021-12-20 19:43:42 +0300

    side effects

--- app/src/main/java/levkaantonov/com/study/composetraining/MainActivity.kt
+++ app/src/main/java/levkaantonov/com/study/composetraining/MainActivity.kt
@@ -2,21 +2,36 @@ package levkaantonov.com.study.composetraining
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.OnBackPressedDispatcher
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.semantics.Role.Companion.Button
 import androidx.compose.ui.tooling.preview.Preview
+import kotlinx.coroutines.delay
 import levkaantonov.com.study.composetraining.ui.theme.ComposeTrainingTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeTrainingTheme {
-                Surface(color = MaterialTheme.colors.background) {
+            val scaffoldState = rememberScaffoldState()
+            Scaffold(scaffoldState = scaffoldState) {
+                var counter by remember {
+                    mutableStateOf(0)
                 }
+
+                if (counter % 5 == 0 && counter > 0) {
+                    LaunchedEffect(key1 = scaffoldState.snackbarHostState) {
+                        scaffoldState.snackbarHostState.showSnackbar(counter.toString())
+                    }
+                }
+
+                if (counter % 5 == 0)
+                    Button(onClick = { counter++ }) {
+                        Text(text = "Click me: ${counter}")
+                    }
             }
         }
     }
