5ac1e7eae490fd492f0eed9b96afceeb7da3f581
levkaantonov <levkaantonov@gmail.com>
2021-12-22 16:16:24 +0300

    added homeScreen

--- app/src/main/java/levkaantonov/com/study/composetraining/MainActivity.kt
+++ app/src/main/java/levkaantonov/com/study/composetraining/MainActivity.kt
@@ -3,6 +3,7 @@ package levkaantonov.com.study.composetraining
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -10,13 +11,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import levkaantonov.com.study.composetraining.ui.theme.ComposeTrainingTheme
 
+@ExperimentalFoundationApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeTrainingTheme {
-                Surface(color = MaterialTheme.colors.background) {
-                }
+                HomeScreen()
             }
         }
     }
