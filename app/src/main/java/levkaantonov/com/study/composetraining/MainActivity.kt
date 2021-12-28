bf8d47f2f55a50f2ae6d8396064f3e657c4996d7
levkaantonov <levkaantonov@gmail.com>
2021-12-28 14:29:39 +0300

    support all screen sizes

--- app/src/main/java/levkaantonov/com/study/composetraining/MainActivity.kt
+++ app/src/main/java/levkaantonov/com/study/composetraining/MainActivity.kt
@@ -3,21 +3,35 @@ package levkaantonov.com.study.composetraining
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import levkaantonov.com.study.composetraining.ui.theme.ComposeTrainingTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeTrainingTheme {
-                Surface(color = MaterialTheme.colors.background) {
-                }
-            }
+//            MediaQuery(Dimensions.Width lessThan 400.dp) {
+//                Text(text = "I'm only show below a width of 400dp")
+//            }
+
+            Text(
+                text = "I'm only show below a width of 400dp",
+                modifier = Modifier
+                    .background(Color.Green)
+                    .mediaQuery(
+                        Dimensions.Width lessThan 400.dp,
+                        modifier = Modifier.size(300.dp)
+                    )
+            )
         }
     }
 }
