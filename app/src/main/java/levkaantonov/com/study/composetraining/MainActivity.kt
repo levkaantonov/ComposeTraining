0accf80d83a86f88b7ff2cee0d949fb84d090a09
levkaantonov <levkaantonov@gmail.com>
2021-12-28 17:50:15 +0300

    simple navigation in compose

--- app/src/main/java/levkaantonov/com/study/composetraining/MainActivity.kt
+++ app/src/main/java/levkaantonov/com/study/composetraining/MainActivity.kt
@@ -3,21 +3,12 @@ package levkaantonov.com.study.composetraining
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import levkaantonov.com.study.composetraining.ui.theme.ComposeTrainingTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeTrainingTheme {
-                Surface(color = MaterialTheme.colors.background) {
-                }
-            }
+            Navigation()
         }
     }
 }
