171473ed2a4cb8092866ea722f40e74a1c7400e0
levkaantonov <levkaantonov@gmail.com>
2022-01-06 12:11:47 +0300

    multi select list

--- app/src/main/java/levkaantonov/com/study/composetraining/MainActivity.kt
+++ app/src/main/java/levkaantonov/com/study/composetraining/MainActivity.kt
@@ -3,11 +3,21 @@ package levkaantonov.com.study.composetraining
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import levkaantonov.com.study.composetraining.ui.theme.ComposeTrainingTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,9 +25,52 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeTrainingTheme {
-                Surface(color = MaterialTheme.colors.background) {
+                var items by remember {
+                    mutableStateOf(
+                        (1..20).map {
+                            ListItem(
+                                title = "Item $it",
+                                isSelected = false
+                            )
+                        }
+                    )
+                }
+                LazyColumn(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    items(items.size) { i ->
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .clickable {
+                                    items = items.mapIndexed { j, item ->
+                                        if (i == j) {
+                                            item.copy(isSelected = !item.isSelected)
+                                        } else item
+                                    }
+                                }
+                                .padding(16.dp),
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Text(text = items[i].title)
+                            if (items[i].isSelected) {
+                                Icon(
+                                    imageVector = Icons.Default.Check,
+                                    contentDescription = "Selected",
+                                    tint = Color.Green,
+                                    modifier = Modifier.size(20.dp)
+                                )
+                            }
+                        }
+                    }
                 }
             }
         }
     }
 }
+
+data class ListItem(
+    val title: String,
+    val isSelected: Boolean
+)
